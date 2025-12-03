2a7546d9f9e7bc6854d9ff2ed8e8d01aff05c9cf
Panji16 <13522028@std.stei.itb.ac.id>
2025-12-03 10:28:32 +0700

    fix duplicate function and variable

--- fuselog_apply/src/main.rs
+++ fuselog_apply/src/main.rs
@@ -9,8 +9,6 @@ use std::fs;
 
 const CACHE_DICT_PATH: &str = "/var/cache/fuselog/statediff.dict";
 
-const CACHE_DICT_PATH: &str = "/var/cache/fuselog/statediff.dict";
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
